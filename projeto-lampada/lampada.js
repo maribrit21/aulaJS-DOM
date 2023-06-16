152af227f34065157883a0ce5acd68c32937503e
maribrit21 <marianacaraujo.21@gmail.com>
2023-06-16 16:31:31 -0300

    update

--- projeto-lampada/lampada.js
+++ projeto-lampada/lampada.js
@@ -1,30 +1,42 @@
-const turnOn = document.getElementById( 'turnOn' )
-const turnOff = document.getElementById( 'turnOff' )
-const lamp = document.getElementById( 'lamp' )
+const turnOn = document.getElementById ( 'turnOn' );
+const turnOff = document.getElementById ( 'turnOff' );
+const turnPink = document.getElementById ( 'turnPink' );
+const lamp = document.getElementById ( 'lamp' );
 
-function isLampBroken() {
-    return lamp.src.indexOf( 'quebrada' ) > -1
+function isLampBroken () {
+    return lamp.src.indexOf ( 'quebrada' ) > -1
 }
 
 function lampOn () {
     if ( !isLampBroken () ) {
-        lamp.src='./img/ligada.jpg';
+        lamp.src = './img/ligada.jpg';
     } 
 }
 
 function lampOff () {
     if ( !isLampBroken () ) {
-        lamp.src='./img/desligada.jpg'
-
+        lamp.src = './img/desligada.jpg'
     }
 }
 
-function lampBroken () {
-    return src='./img/quebrada.jpg';
+function lampPink () {
+    if ( !isLampBroken () ) {
+        lamp.src = './img/rosa.jpg.jpg';
+    } 
 }
 
-turnOn.addEventListener('click', lampOn);
-turnOff.addEventListener('click', lampOff);
-lamp.addEventListener('mouseover', lampOn)
-lamp.addEventListener('mouseleave', lampOff)
-lamp.addEventListener('dblclick', lampBroken)
+function lampBroken () {
+    lamp.src = './img/quebrada.jpg';
+}
+
+function changeColor () {
+    lamp.src = './img/rosa.jpg.jpg'
+}
+
+
+turnOn.addEventListener( 'click', lampOn );
+turnOff.addEventListener( 'click', lampOff );
+turnPink.addEventListener( 'click', lampPink );
+lamp.addEventListener( 'mouseover', changeColor );
+lamp.addEventListener( 'mouseleave', lampOff );
+lamp.addEventListener( 'dblclick', lampBroken );
